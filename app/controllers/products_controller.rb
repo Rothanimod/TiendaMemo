b861b291f9149762db93b319cae2727f2f098c0d
Mauricio Noreña <baphometh@gmail.com>
2016-10-21 20:21:15 -0500

    added bootstrap classes to the create view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,17 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = 'The product has been saved'
+      redirect_to @product
+    else
+      @erros = @product.erros.full_messages
+      render :new
+    end
+  end
+
   def edit
   end
 
@@ -13,5 +24,11 @@ class ProductsController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :reference, :price, :quantity, :brand, :description)
   end
 end
