5891104efa98e246cf8b4a147d28c5102f00787a
Mauricio Noreña <baphometh@gmail.com>
2016-11-17 20:11:00 -0500

    added letteropener as mailer for develpment environment

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
     resources :categories
     resources :products
 
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
